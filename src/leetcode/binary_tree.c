75438088b0c518f54705b71575adac15ec6a86e5
pipl <pipl@localhost.localdomain>
2020-09-23 06:17:32 -0400

    feat:添加脚本构建，进一步完善二叉树DFS

--- src/leetcode/binary_tree.c
+++ src/leetcode/binary_tree.c
@@ -7,6 +7,24 @@ typedef struct _TreeNode {
   struct _TreeNode *right;
 } TreeNode;
 
+void DFS(char **strPath, int path[1000], int depth, int *size, TreeNode *node) {
+  if (node == NULL)
+    return;
+  path[depth] = node->val;
+  ++depth;
+  if (node->left == NULL && node->right == NULL) { //遍历到最后的一个节点
+    char *subPath = malloc(1000);
+    for (int i = 0; i < depth; i++) {
+      sprintf(subPath, "%s->%d", subPath, path[i]);
+    }
+    strPath[*size] = subPath;
+    *size += 1;
+    return;
+  }
+  DFS(strPath, path, depth, size, node->left);
+  DFS(strPath, path, depth, size, node->right);
+}
+
 /**
  * @brief 获取二叉树的从根节点到叶子节点的路径
  *
@@ -15,8 +33,22 @@ typedef struct _TreeNode {
  *
  * @return 1->4->5字符串路径数组
  */
-char **binaryTreePaths(TreeNode *root, int *returnSize) {}
+char **binaryTreePaths(TreeNode *root, int *returnSize) {
+  char **strPath = malloc(1000 * sizeof(char *));
+  int path[1000] = {0};
+  int depth = 0;
+  *returnSize = 0;
+  DFS(strPath, path, depth, returnSize, root);
+  return path;
+}
 
+/**
+ * @brief 构建一个二叉树节点
+ *
+ * @param val
+ *
+ * @return 
+ */
 TreeNode *CreateNode(int val) {
   TreeNode *node = (TreeNode *)malloc(sizeof(TreeNode));
   node->val = val;
@@ -25,6 +57,12 @@ TreeNode *CreateNode(int val) {
   return node;
 }
 
+/**
+ * @brief 往二叉树添加节点
+ *
+ * @param root
+ * @param val
+ */
 void BinaryTreeAddNode(TreeNode *root, int val) {
   if (root == NULL)
     return;
@@ -41,19 +79,30 @@ void BinaryTreeAddNode(TreeNode *root, int val) {
   (tmp->val > val) ? (tmp->left = new) : (tmp->right = new);
 }
 
+/**
+ * @brief 中序遍历
+ *
+ * @param root
+ */
 void Print(TreeNode *root) {
 
+  if (root == NULL)
+    return;
   TreeNode *node = root;
+  Print(node->left);
   if (node != NULL)
     printf("%d ->", node->val);
-  else
-    return;
-  Print(node->left);
   Print(node->right);
 }
 
-void CreateBinaryTree(TreeNode *root) {
-  for (int i = 0; i < 20; i++) {
+/**
+ * @brief 构建随机二叉树
+ *
+ * @param root
+ * @param num：节点个数
+ */
+void CreateBinaryTree(TreeNode *root , int num) {
+  for (int i = 0; i < num; i++) {
     int val = rand() % 100;
     BinaryTreeAddNode(root, val);
   }
@@ -61,7 +110,14 @@ void CreateBinaryTree(TreeNode *root) {
 
 int main() {
   TreeNode *root = CreateNode(50);
-  CreateBinaryTree(root);
+  CreateBinaryTree(root, 20);
   Print(root);
+  int size = 0;
+  char **path = binaryTreePaths(root, &size);
+  for (int i = 0; i < size; i++) {
+    printf(path[i]);
+    free(path[i]);
+  }
+  free(path);
   return 0;
 }
---
+++ src/leetcode/build.sh
@@ -0,0 +1,2 @@
+# binary_tree
+gcc -g binary_tree.c -o binary_tree --std=c99
